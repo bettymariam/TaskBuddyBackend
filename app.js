fecf6b38619ea15efac08f79119b6bd1d699dc39
Betty Chempananical <bettymariam@gmail.com>
2017-07-11 12:42:48 -0700

    Added a privacy policy

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var mrewards = require('./routes/mrewards');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('Welcome');
 });
 
+router.get('/privacy', function(req, res, next) {
+  res.render('policy');
+});
+
 module.exports = router;
