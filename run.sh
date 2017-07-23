8a9035a5dd152b1533f6055944b464df8e5654a8
Betty Chempananical <bettymariam@gmail.com>
2017-07-23 16:14:58 -0700

    updated port in script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-PORT=443 \
+PORT=80 \
 NODE_ENV=production \
 SERVER_URL=https://ec2-34-198-223-197.compute-1.amazonaws.com \
 DATABASE_URL=postgres://tasksadmin:tasksadmin@tasks-db.c9o41mpvtl2p.us-east-1.rds.amazonaws.com:5432/tasks_db \
