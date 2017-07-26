9fe5903d34de59cd9f6533d458701df0c9fb287c
Betty Chempananical <bettymariam@gmail.com>
2017-07-26 10:15:19 -0700

    updated seeds

--- db/seeds/11_claimed_rewards.js
+++ db/seeds/11_claimed_rewards.js
@@ -16,6 +16,20 @@ exports.seed = function(knex, Promise) {
          reward_id: 2,
          created_at: new Date('2017-07-11 14:26:16 UTC'),
          updated_at: new Date('2017-07-11 14:26:16 UTC')
+       },
+       {
+         id: 3,
+         member_id: 1,
+         reward_id: 3,
+         created_at: new Date('2017-07-11 14:26:16 UTC'),
+         updated_at: new Date('2017-07-11 14:26:16 UTC')
+       },
+       {
+         id: 4,
+         member_id: 1,
+         reward_id: 4,
+         created_at: new Date('2017-07-11 14:26:16 UTC'),
+         updated_at: new Date('2017-07-11 14:26:16 UTC')
         }
       ]);
     })
